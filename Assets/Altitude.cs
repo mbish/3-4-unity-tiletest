242c25371b8d96210823d3a03597efd38a791fe3
Mark Bishop <mbishop1990@gmail.com>
2024-02-04 11:00:21 -0800

    wip

--- Assets/Altitude.cs
+++ Assets/Altitude.cs
@@ -8,18 +8,6 @@ public class Altitude : MonoBehaviour
     public Collider2D objectBase;
     private Vector2 startingPosition;
 
-    T CopyComponent<T>(T original, GameObject destination) where T : Component
-    {
-        System.Type type = original.GetType();
-        Component copy = destination.AddComponent(type);
-        System.Reflection.FieldInfo[] fields = type.GetFields();
-        foreach (System.Reflection.FieldInfo field in fields)
-        {
-            field.SetValue(copy, field.GetValue(original));
-        }
-        return copy as T;
-    }
-
     void Awake() {
         startingPosition = new Vector2(objectBase.offset.x, objectBase.offset.y);
         adjustCollider();
